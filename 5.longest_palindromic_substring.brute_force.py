64405c8e54d2c46e1028cefd749ee962f689a2b2
Darren Sadr <darrensadr@gmail.com>
2025-06-22 12:26:24 -0700

    5.longest_palindromic_substring.brute_force.py

--- 5.longest_palindromic_substring.brute_force.py
+++ 5.longest_palindromic_substring.brute_force.py
@@ -1,19 +1,9 @@
-# Approach: Two Pointer
-# Time Complexity: O(n^3); n^2 for generating each substring and n for palindrome verification
-# Verdict: Pass (!?)
-
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res = ""
-
-        def isPalindrome(s: str) -> bool:
-            # Any implementation from "125. Valid Palindrome"
-            return s[::-1] == s
-
+        res = ''
         for i in range(len(s)):
-            for j in range(i+1, len(s)):
-                substring = s[i:j]
-                if isPalindrome(substring) and len(substring) > len(res):
-                    res = substring
-
+            for j in range(i+1, len(s)+1):
+                p = s[i:j]
+                if p[::-1] == p and len(p) > len(res):
+                    res = p
         return res
