08e9c5ab004e6a5f61d63197e4889173b2c0fa9b
Darren Sadr <darrensadr@gmail.com>
2025-06-22 10:28:15 -0700

    268.missing_number.py

--- 268.missing_number.py
+++ 268.missing_number.py
@@ -4,9 +4,7 @@
 class Solution:
     def missingNumber(self, nums: list[int]) -> int:
         nums = sorted(nums)
-        if nums[0] != 0:
-            return 0
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i-1]+1:
-                return nums[i-1]+1
+        for i in range(len(nums)):
+            if nums[i] != i:
+                return i
         return nums[-1]+1
