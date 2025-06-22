9b215aa3f45ca97c757a99dd9af33ec591711c91
Darren Sadr <darrensadr@gmail.com>
2025-06-22 15:50:53 -0700

    two sum

--- 1.two_sum.hashmap.py
+++ 1.two_sum.hashmap.py
@@ -1,13 +1,10 @@
-# HashMap: Another example of use case
-
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         remainders = dict()
-        for i in range(len(nums)):
-            n = nums[i]
+        for i,n in enumerate(nums):
             if n in remainders:
                 return [i, remainders[n]]
             else:
                 remainders[target - n] = i
-        # Problem states assumption that all inputs have one solution
+        # should not arrive here
         return []
