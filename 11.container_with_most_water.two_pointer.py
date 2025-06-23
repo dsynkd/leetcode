5fc27d5c8823752b76e86ae7456d250c8e35feba
Darren Sadr <darrensadr@gmail.com>
2025-06-23 15:01:44 -0700

    container_with_most_water

--- 11.container_with_most_water.two_pointer.py
+++ 11.container_with_most_water.two_pointer.py
@@ -1,17 +1,12 @@
-# Approach: Two Pointer, most baic case
-
 class Solution:
     def maxArea(self, height: list[int]) -> int:
-        max_area = 0
         i, j = 0, len(height)-1
-        while i != j:
-            h = min(height[i], height[j])
-            w = j - i
-            area = h * w
-            if area > max_area:
-                max_area = area
-            if height[i] < height[j]:
+        res = 0
+        while i < j:
+            area = min(height[i], height[j]) * (j-i)
+            res = max(res, area)
+            if height[j] > height[i]:
                 i += 1
             else:
                 j -= 1
-        return max_area
+        return res
