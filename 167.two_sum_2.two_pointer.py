6d156df685794be6d19a90af283432392bba17ff
Darren Sadr <darrensadr@gmail.com>
2025-06-23 14:37:34 -0700

    .

--- 167.two_sum_2.two_pointer.py
+++ 167.two_sum_2.two_pointer.py
@@ -2,10 +2,9 @@ class Solution:
     def twoSum(self, numbers: list[int], target: int) -> list[int]:
         i,j = 0, len(numbers)-1
         while (sum := numbers[i] + numbers[j]) != target:
-            assert(i < j)
+            assert(i < j) # problem states each test case must have one solution
             if sum > target:
                 j -= 1
             else:
                 i += 1
-        # +1 because it is asked by problem
-        return [i+1,j+1]
+        return [i+1,j+1] # +1 because problem asks for 1-based index
