75b3f55963dcdaccc67da3e1dbbd38435ef1179e
Darren Sadr <darrensadr@gmail.com>
2025-06-23 14:34:06 -0700

    3sum

--- 15.three_sum.two_pointer.py
+++ 15.three_sum.two_pointer.py
@@ -1,38 +1,24 @@
-# Similar to 'Memoization' solution, but we sort the array initially to prevent having to sort it for
-# each obtained subset
-# Time Complexity: O(n^2) 
-
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        nums = sorted(nums)
-        memo = dict()
-
-        def twoSum(nums: list[int], target: int):
-            output = []
-            remainders = dict()
-            for n in nums:
-                remainder = target - n
-                if n in remainders:
-                    output.append([n, remainders[n]])
-                remainders[remainder] = n
-            return output
-
-        # Using set to prevent duplicates
-        output = set()
-
-        # For every item in nums, set target = 0-num and run two sum on the remainder of the array
-        # Cache the results for each run so we do not have to repeat the same operation for repeated numbers
+        nums.sort()
+        res = []
         for i in range(len(nums)):
-            n = nums[i]
-            if n in memo:
+            # skip over duplicates
+            if i > 0 and nums[i-1] == nums[i]:
                 continue
-            res = twoSum(nums[:i] + nums[i+1:], -n)
-            # Add the number back into the combinations
-            for j in range(len(res)):
-                res[j] += [n]
-                # Need to sort to detect duplicates to work
-                output.add(tuple(sorted(res[j])))
-
-            memo[n] = True
-
-        return [list(a) for a in output]
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                sum = nums[j] + nums[k] + nums[i]
+                if sum == 0:
+                    res.append([nums[i],nums[j],nums[k]])
+                    k -= 1
+                    j += 1
+                    # skip over duplicates
+                    while j < k and nums[j] == nums[j-1]:
+                        j += 1
+                elif sum > 0:
+                    k -= 1
+                else:
+                    j += 1
+        return res
