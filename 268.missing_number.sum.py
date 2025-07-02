c5e7fbc056318030fa54e2df1d94f0dbb9414a9e
Darren Sadr <darrensadr@gmail.com>
2025-07-02 16:13:19 -0700

    missing number

--- 268.missing_number.sum.py
+++ 268.missing_number.sum.py
@@ -1,7 +1,3 @@
-# Time Complexity: O(n)
-# Space Complexity: O(1)
-# Verdict: Pass
-
 class Solution:
     def missingNumber(self, nums: list[int]) -> int:
         n = len(nums)
--- 268.missing_number.sum_guass.py
+++ 268.missing_number.sum_guass.py
@@ -1,8 +1,4 @@
-# Time Complexity: O(n)
-# Space Complexity: O(1)
-# Verdict: Pass
-
 class Solution:
     def missingNumber(self, nums: list[int]) -> int:
-        n = len(nums)
+        n = len(nums)+1
         return n*(n+1)//2 - sum(nums)
