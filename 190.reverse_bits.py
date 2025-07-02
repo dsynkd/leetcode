b24781c02328a63894b0309676ea779c756542af
Darren Sadr <darrensadr@gmail.com>
2025-07-02 16:45:48 -0700

    reverse bits

--- 190.reverse_bits.py
+++
@@ -1,7 +0,0 @@
-# Approach: Brute Force
-
-class Solution:
-    def reverseBits(self, n: int) -> int:
-        b = bin(n)[2:]
-        b = (32-len(b)) * '0' + b
-        return int(''.join(reversed(b)), 2)
