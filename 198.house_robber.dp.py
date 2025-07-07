118b55149da34e9a541074ffe7648dc8a889f205
Darren Sadr <darrensadr@gmail.com>
2025-07-07 16:26:40 -0700

    house robber

--- 198.house_robber.dp.py
+++ 198.house_robber.dp.py
@@ -1,15 +1,11 @@
-# Approach: Dynamic Programming
-# Time Complexity: O(n) where n = len(nums)
-# Verdict: Pass
-
 class Solution:
     def rob(self, nums: list[int]) -> int:
-        n = len(nums)
-        if n < 3:
+        N = len(nums)
+        if N < 3:
             return max(nums)
-        dp = [0] * n
+        dp = [0] * N
         dp[0] = nums[0]
         dp[1] = max(nums[0], nums[1])
-        for i in range(2, n):
+        for i in range(2, N):
             dp[i] = max(dp[i-2] + nums[i], dp[i-1])
         return dp[-1]
