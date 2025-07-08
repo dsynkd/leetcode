55b21afb053188d512d2b871971797d247d4dead
Darren Sadr <darrensadr@gmail.com>
2025-07-07 21:27:45 -0700

    house robber 2

--- 213.house_robber_2.dp.py
+++ 213.house_robber_2.dp.py
@@ -1,22 +1,13 @@
-# Approach: Dynamic Programming
-# Time Complexity: O(n) where n = len(nums)
-# Verdict: Pass
-
 class Solution:
     def rob(self, nums: list[int]) -> int:
-        n = len(nums)
-        if n < 4:
+        N = len(nums)
+        if N < 4:
             return max(nums)
-        # Maintain two DP arrays:
-        # 1. When we rob the first house and not the last
-        # 2. When we rob the last house and not the first
-        dp1 = [0] * n
-        dp2 = [0] * n
-        dp1[0] = nums[0]
-        dp1[1] = max(nums[0], nums[1])
-        dp2[0] = 0
-        dp2[1] = nums[1]
-        for i in range(2, n):
+        dp1 = [0] * N
+        dp2 = [0] * N
+        dp1[0], dp1[1] = nums[0], max(nums[0], nums[1])
+        dp2[0], dp2[1] = 0, nums[1]
+        for i in range(2, N):
             dp1[i] = max(dp1[i-2] + nums[i], dp1[i-1])
             dp2[i] = max(dp2[i-2] + nums[i], dp2[i-1])
         return max(dp1[-2], dp2[-1])
