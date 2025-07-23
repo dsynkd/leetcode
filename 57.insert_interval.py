a0c447df81f581bcb5f148121d98e43f8ac87b91
Darren Sadr <darrensadr@gmail.com>
2025-07-23 16:24:27 -0600

    insert interval

--- 57.insert_interval.py
+++ 57.insert_interval.py
@@ -1,15 +1,12 @@
 class Solution:
     def insert(self, intervals: list[list[int]], newInterval: list[int]) -> list[list[int]]:
-        b = newInterval
         res = []
-        for i in range(len(intervals)):
-            a = intervals[i]
-            if b[0] > a[1]:
+        n = newInterval
+        for i,a in enumerate(intervals):
+            if a[1] < n[0]:
                 res.append(a)
-            elif b[1] < a[0]:
-                res.append(b)
-                return res + intervals[i:]
+            elif a[0] <= n[1]:
+                n = [min(a[0], n[0]), max(a[1], n[1])]
             else:
-                b = [min(a[0], b[0]), max(a[1], b[1])]
-        res.append(b)
-        return res
+                return res + [n] + intervals[i:]
+        return res + [n]
