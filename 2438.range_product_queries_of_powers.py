f297ecd37c0c83ec04a3404dce02a6bcf6503969
Darren Sadr <darrensadr@gmail.com>
2025-08-11 14:47:51 -0700

    35, 2438

---
+++ 2438.range_product_queries_of_powers.py
@@ -0,0 +1,21 @@
+class Solution:
+    def productQueries(self, n: int, queries: list[list[int]]) -> list[int]:
+        powers = []
+        p = 1
+        while p <= n:
+            p *= 2
+        p //= 2
+        while p > 0:
+            a = n // p
+            if a >= 1:
+                powers += [p] * a
+            n %= p
+            p //= 2
+        powers = powers[::-1]
+        res = []
+        for i,j in queries:
+            a = 1
+            for k in range(i,j+1):
+                a *= powers[k]
+            res.append(a % (pow(10,9)+7))
+        return res
