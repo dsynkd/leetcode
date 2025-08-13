729fe1354d55328adeac1f0693359eb9f0f5e367
Darren Sadr <darrensadr@gmail.com>
2025-08-13 12:37:00 -0700

    .

--- 36.valid_sudoku.py
+++ 36.valid_sudoku.py
@@ -11,18 +11,18 @@ class Solution:
                 if board[i][j] == '.':
                     continue
                 num = int(board[i][j])
+                
                 if num in rows[i]:
                     return False
-                else:
-                    rows[i].add(num)
+                rows[i].add(num)
+                
                 if num in cols[j]:
                     return False
-                else:
-                    cols[j].add(num)
+                cols[j].add(num)
+                
                 # Get index of square
                 k = (i//3)*3 + j//3
                 if num in squares[k]:
                     return False
-                else:
-                    squares[k].add(num)
+                squares[k].add(num)
         return True
