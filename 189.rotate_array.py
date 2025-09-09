aeb64b2d55db8cd95bcc8f2b1c40ded37f679e6a
Darren Sadr <darrensadr@gmail.com>
2025-09-09 15:37:32 -0700

    189

--- 189.rotate_array.py
+++ 189.rotate_array.py
@@ -1,7 +1,4 @@
 class Solution:
     def rotate(self, nums: list[int], k: int) -> None:
-        res = [0] * len(nums)
-        for i,n in enumerate(nums):
-            res[(i+k)%len(nums)] = n
-        for i,n in enumerate(res):
-            nums[i] = n
+        for _ in range(k):
+            nums.insert(0, nums.pop())
