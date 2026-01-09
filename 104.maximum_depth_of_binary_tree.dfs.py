6de5653ec1eab1749acc5c202f4ad6d1bdb7bd59
Darren Sadr <darrensadr@gmail.com>
2026-01-08 19:44:42 -0800

    104

--- 104.maximum_depth_of_binary_tree.dfs.py
+++ 104.maximum_depth_of_binary_tree.dfs.py
@@ -8,14 +8,10 @@ class TreeNode:
 
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        self.max = 0
 
         def dfs(node: Optional[TreeNode], depth: int):
-            if node is None:
-                self.max = max(self.max, depth)
-                return
-            dfs(node.left, depth + 1)
-            dfs(node.right, depth + 1)
+            if not node:
+                return depth
+            return max(dfs(node.left, depth + 1), dfs(node.right, depth + 1))
         
-        dfs(root, 0)
-        return self.max
+        return dfs(root, 0)
