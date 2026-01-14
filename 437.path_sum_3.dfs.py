4712603d30bf6770eba4aee77f7335dbdf19c0d0
Darren Sadr <darrensadr@gmail.com>
2026-01-14 14:25:39 -0800

    560 + 437

--- 437.path_sum_3.dfs.py
+++ 437.path_sum_3.dfs.py
@@ -1,4 +1,3 @@
-# Approach: DFS
 from typing import Optional
 
 class TreeNode:
@@ -9,8 +8,6 @@ class TreeNode:
 
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        self.paths = 0
-
         def dfs(tree: Optional[TreeNode], sum: int) -> int:
             if tree is None:
                 return 0
