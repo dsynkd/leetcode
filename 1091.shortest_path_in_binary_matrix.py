8b80b1ce37548656c6d7390e93a15a079fb4fa24
Darren Sadr <darrensadr@gmail.com>
2026-01-18 14:25:23 -0800

    1091

--- 1091.shortest_path_in_binary_matrix.py
+++ 1091.shortest_path_in_binary_matrix.py
@@ -1,28 +1,35 @@
 from collections import deque
 
-
 class Solution:
     def shortestPathBinaryMatrix(self, grid: list[list[int]]) -> int:
         n = len(grid)
+        assert(n == len(grid[0]))
+        if grid[0][0] == 1:
+            return -1
+        Q = deque()
+        Q.append((0,0,1))
         visited = set()
-        q = deque()
-        q.append((0,0,1))
 
-        while q:
-            (i,j,c) = q.popleft()
-            if (i,j) == (n-1,n-1) and grid[i][j] == 0:
-                return c
+        while Q:
+            (i,j,d) = Q.popleft()
+            if (i,j) == (n-1,n-1):
+                if grid[i][j] == 1:
+                    return -1
+                return d
             
-            if (not 0 <= i < n) or (not 0 <= j < n) or (i,j) in visited or grid[i][j] == 1:
+            if (not 0 <= i < n) or (not 0 <= j < n):
+                continue
+            if (i,j) in visited or grid[i][j] == 1:
                 continue
             visited.add((i,j))
-            q.append((i+1,j+1,c+1))
-            q.append((i+1,j,c+1))
-            q.append((i,j+1,c+1))
-            q.append((i+1,j-1,c+1))
-            q.append((i-1,j+1,c+1))
-            q.append((i-1,j,c+1))
-            q.append((i,j-1,c+1))
-            q.append((i-1,j-1,c+1))
+            d += 1
+            Q.append((i+1, j+1, d))
+            Q.append((i+1, j,   d))
+            Q.append((i,   j+1, d))
+            Q.append((i+1, j-1, d))
+            Q.append((i-1, j+1, d))
+            Q.append((i-1, j,   d))
+            Q.append((i,   j-1, d))
+            Q.append((i-1, j-1, d))
         
         return -1
