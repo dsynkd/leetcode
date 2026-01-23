49bad3b07c6082ada78074e9818aeac5db536b14
Darren Sadr <darrensadr@gmail.com>
2026-01-23 13:56:54 -0800

    490

---
+++ 490.the_maze.dfs.py
@@ -0,0 +1,29 @@
+class Solution:
+    def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
+        m = len(maze)
+        n = len(maze[0])
+        D = [(1,0), (-1,0), (0,1), (0,-1)]
+        visited = set()
+
+        def dfs(x,y):
+            assert((0 <= x < m) and (0 <= y < n) and maze[x][y] == 0)
+            
+            if (x,y) == tuple(destination):
+                return True
+            
+            if (x,y) in visited:
+                return False
+            visited.add((x,y))
+            
+            for dx,dy in D:
+                i,j = x,y
+                while (0 <= i+dx < m) and (0 <= j+dy < n) and maze[i+dx][j+dy] == 0:
+                    i += dx
+                    j += dy
+                if dfs(i,j):
+                    return True
+            
+            return False
+        
+        i,j = start
+        return dfs(i, j)
