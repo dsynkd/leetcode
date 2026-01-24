e32f3c45bf94a5a77e20dca6e877f85c3721dc29
Darren Sadr <darrensadr@gmail.com>
2026-01-24 12:03:30 -0800

    1065

--- 1065.index_pairs_of_string.prefix_tree.py
+++ 1065.index_pairs_of_string.prefix_tree.py
@@ -1,9 +1,30 @@
+class Node:
+    def __init__(self):
+        self.children = dict()
+        self.endOfWord = False
+
 class Solution:
     def indexPairs(self, text: str, words: list[str]) -> list[list[int]]:
-        wordSet = set(words)
+        # Populate the tree
+        root = Node()
+        for word in words:
+            node = root
+            for c in word:
+                if c not in node.children:
+                    node.children[c] = Node()
+                node = node.children[c]
+            node.endOfWord = True
+        
+        # Search through text
+        i = 0
         res = []
         for i in range(len(text)):
-            for j in range(i, len(text)):
-                if text[i:j+1] in wordSet:
+            node = root
+            j = i
+            while j < len(text) and text[j] in node.children:
+                node = node.children[text[j]]
+                if node.endOfWord:
                     res.append([i,j])
+                j += 1
+
         return res
