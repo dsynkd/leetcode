1fb0bc448109d12a5756025fc8690d8badf340ca
Darren Sadr <darrensadr@gmail.com>
2026-04-08 20:10:31 -0700

    88

--- 88.merge_sorted_array.py
+++ 88.merge_sorted_array.py
@@ -2,8 +2,9 @@ class Solution:
     def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
         j = i = 0
         tmp = nums1.copy()
+
         for x in range(m+n):
-            if not j < len(nums2) or i < m and tmp[i] < nums2[j]:
+            if j >= n or (i < m and tmp[i] < nums2[j]):
                 nums1[x] = tmp[i]
                 i += 1
             else:
